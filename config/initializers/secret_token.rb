99e1de25872d002fd54f848575d6cd0af99b4c76
Julian Skinner <julian@jussmedia.com>
2014-03-20 12:01:47 -0400

    Rails baseline app structure.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZombieTwitter::Application.config.secret_key_base = 'ebb8166a95edf47a37abe711c8153d47ce06c796745660751d4ee734bb70a02f586147c80cad344c7580a9da2dbb4c364704c6d097c54088b1f4002c9baeb70c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ZombieTwitter::Application.config.session_store :cookie_store, key: '_zombie_twitter_session'
